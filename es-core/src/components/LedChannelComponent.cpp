4cfe022a49a07fdcbd94f881948fec4fbb4331f9
MisterXYZ83 <marco.virgulti@gmail.com>
2022-01-08 14:52:12 +0100

    Update LedChannelComponent.cpp

--- es-core/src/components/LedChannelComponent.cpp
+++ es-core/src/components/LedChannelComponent.cpp
@@ -6,8 +6,6 @@
 #define MOVE_REPEAT_DELAY 500
 #define MOVE_REPEAT_RATE 40
 
-SliderComponent(Window* window, float min, float max, float increment, const std::string& suffix = "");
-
 LedChannelComponent::LedChannelComponent(Window* window, int channel) : SliderComponent(window, 0.0f, 255.0f, 1.0f, "/255"), mMoveRate(0), mKnob(window), mSuffix(suffix)
 {
 	mChannel = channel;
